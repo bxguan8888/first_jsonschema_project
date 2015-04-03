8192d51be6e5d6fc859877a05a8f5990a1c77e2d
Boxuan Guan <boxuanguan@gmail.com>
2015-04-03 10:40:44 -0400

    draft

--- validator.backup.rb
+++ validator.backup.rb
@@ -3,29 +3,6 @@ require 'json-schema'
 require 'json'
 
 
-
-IP_json_schema={
-	"type" => "object",
-	"properties" => {
-		"suncToken"=> {"type" => "string"},         #TODO(boxuguan): add value constraint				"createDate" => {"type" => "string"},
-		"prefixes" => {
-			"type" => "array",
-			"items" => {
-				"oneOf"=>[
-					{"$ref" => "IPPrefix/InternetEC2.json#/definition"},
-					{"$ref" => "IPPrefix/BorderEC2.json#/definition"},
-					{"$ref" => "IPPrefix/AZEC2.json#/definition"},
-					{"$ref" => "IPPrefix/CLOUDFRONT.json#/definition"},
-					{"$ref" => "IPPrefix/DX.json#/definition"},
-					{"$ref" => "IPPrefix/INGEST.json#/definition"},
-					{"$ref" => "IPPrefix/PROD.json#/definition"},
-					{"$ref" => "IPPrefix/ROUTE53.json#/definition"}
-				]
-			}
-		}
-	}
-}
-
 # IP_json_schema2={"$ref" => "IPPrefix/InternetEC2.json#/definition"}
 IP_json_schema2={"$ref" => "IPPrefix.json#/definition"}
 
@@ -67,3 +44,28 @@ end
 
 # errors = JSON::Validator.fully_validate(schema, data)
 # puts errors
+
+
+
+
+# IP_json_schema={
+# 	"type" => "object",
+# 	"properties" => {
+# 		"suncToken"=> {"type" => "string"},         #TODO(boxuguan): add value constraint				"createDate" => {"type" => "string"},
+# 		"prefixes" => {
+# 			"type" => "array",
+# 			"items" => {
+# 				"oneOf"=>[
+# 					{"$ref" => "IPPrefix/InternetEC2.json#/definition"},
+# 					{"$ref" => "IPPrefix/BorderEC2.json#/definition"},
+# 					{"$ref" => "IPPrefix/AZEC2.json#/definition"},
+# 					{"$ref" => "IPPrefix/CLOUDFRONT.json#/definition"},
+# 					{"$ref" => "IPPrefix/DX.json#/definition"},
+# 					{"$ref" => "IPPrefix/INGEST.json#/definition"},
+# 					{"$ref" => "IPPrefix/PROD.json#/definition"},
+# 					{"$ref" => "IPPrefix/ROUTE53.json#/definition"}
+# 				]
+# 			}
+# 		}
+# 	}
+# }
--- validator.rb
+++ validator.rb
@@ -2,15 +2,57 @@ require 'rubygems'
 require 'json-schema'
 require 'json'
 
-IP_json_schema={"$ref" => "IPPrefix.json#/definition"}
+IP_json_schema={
+	"type" => "object",
+	"properties" => {
+		"suncToken"=> {"type" => "string"},
+		"createDate"=>{"type" => "string"},
+		"prefixes" => {
+			"type" => "array",
+			"items" => {
+				"oneOf"=> [
+					{"$ref" => "IPPrefix/InternetEC2.json#/definition"},
+					{"$ref" => "IPPrefix/BorderEC2.json#/definition"},
+					{"$ref" => "IPPrefix/AZEC2.json#/definition"},
+					{"$ref" => "IPPrefix/CLOUDFRONT.json#/definition"},
+					{"$ref" => "IPPrefix/DX.json#/definition"},
+					{"$ref" => "IPPrefix/INGEST.json#/definition"},
+					{"$ref" => "IPPrefix/PROD.json#/definition"},
+					{"$ref" => "IPPrefix/ROUTE53.json#/definition"}
+				]
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+# IP_json_schema={"$ref" => "IPPrefix.json#/definition"}
 
 data = File.read('input.json')
 json_data = JSON.parse(data)
 
 
-# puts json_data
-if JSON::Validator.validate(IP_json_schema, json_data)
-	puts "yes" 
-else
-	puts "No"	
-end 
+# # puts json_data
+# if JSON::Validator.validate(IP_json_schema, json_data)
+# 	puts "yes" 
+# else
+# 	puts "No"	
+# end 
+
+
+# begin
+#   JSON::Validator.validate!(IP_json_schema, json_data)
+# rescue JSON::Schema::ValidationError
+#   puts $!.message
+# end
+
+
+# errors = JSON::Validator.fully_validate(IP_json_schema, json_data)
+
+errors = JSON::Validator.fully_validate(IP_json_schema, json_data)
+puts errors
