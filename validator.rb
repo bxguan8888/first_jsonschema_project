d019b02c79e065ece3e1106174356753dc36f644
Boxuan Guan <boxuanguan@gmail.com>
2015-05-03 16:56:45 -0400

    revision

--- validator.rb
+++ validator.rb
@@ -26,19 +26,63 @@ IP_json_schema={
 			}
 		}
 	},
-	"additionalProperties" => {"type" => "false"}
+	"additionalProperties" => false
 }
 
 
 
 
+prefixes = {
+			"type" => "array",
+			"items" => {
+				"oneOf"=> [
+					# {"$ref" => "IPPrefix/InternetEC2.json#/definition"},
+					# {"$ref" => "IPPrefix/BorderEC2.json#/definition"},
+					# {"$ref" => "IPPrefix/AZEC2.json#/definition"},
+					# {"$ref" => "IPPrefix/CLOUDFRONT.json#/definition"},
+					# {"$ref" => "IPPrefix/DX.json#/definition"},
+					# {"$ref" => "IPPrefix/INGEST.json#/definition"},
+					# {"$ref" => "IPPrefix/PROD.json#/definition"},
+					{"$ref" => "IPPrefix/ROUTE53.json#/definition"},
+					{"$ref" => "IPPrefix/generic.json#/definition"}
+				]
+				# "type" => {"$ref" => "IPPrefix/generic.json#/definition"}
+			}
+		}
 
 
 
-# IP_json_schema={"$ref" => "IPPrefix.json#/definition"}
+
+# data = File.read('input.json')
+# json_data = JSON.parse(data)
+
+# errors = JSON::Validator.fully_validate(IP_json_schema, json_data)
+# if errors.empty?
+# 	puts true
+# else
+# 	raise StandardError,  (puts errors)
+# end
+
+
 
 data = File.read('input.json')
 json_data = JSON.parse(data)
+
+# #method 1 to load json schema
+# prefixes = File.read('prefix.json')
+# prefixes = JSON.parse(prefixes)
+
+#method 2 to load json schema
+prefixes = {"$ref" => "prefix.json#/"}
+
+puts prefixes
+
+errors = JSON::Validator.fully_validate(prefixes, json_data)
+if errors.empty?
+	puts true
+else
+	raise StandardError,  (puts errors)
+end
 
 
 # # puts json_data
@@ -57,9 +101,3 @@ json_data = JSON.parse(data)
 
 
 
-errors = JSON::Validator.fully_validate(IP_json_schema, json_data)
-if errors.empty?
-	puts true
-else
-	raise StandardError,  (puts errors)
-end
